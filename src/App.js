765e960739ad6deb5d244ba0ae8adc15a0327c72
ahmetaydar <a.aydar2014@gmail.com>
2023-04-13 18:50:39 +0300

    release

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Square from "./Square";
+import Input from "./Input";
+import { useState } from "react";
+
+const App = () => {
+  const [colorValue, setColorValue] = useState("");
+  const [hexValue, setHexValue] = useState("");
+  const [isDarkText, setIsDarkText] = useState(true);
+
+  return (
+    <div className="App">
+      <Square
+        colorValue={colorValue}
+        hexValue={hexValue}
+        isDarkText={isDarkText}
+      />
+      <Input
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+        hexValue={hexValue}
+        setHexValue={setHexValue}
+        isDarkText={isDarkText}
+        setIsDarkText={setIsDarkText}
+      />
+    </div>
+  );
+};
+
+export default App;
